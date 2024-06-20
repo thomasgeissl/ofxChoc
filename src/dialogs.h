ef00dc3670713e2db5ccd5a2f9425c6b8c966d70
Thomas Geissl <thomas.geissl@gmail.com>
2024-06-20 15:23:22 +0200

    some dialog improvements

--- src/dialogs.h
+++ src/dialogs.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "ofThread.h"
+#include "ofColor.h"
 #include <string>
 #include <functional>
 #include <iostream>
-#include <array>
 
 extern "C" {
 #include "tinyfiledialogs.h"
@@ -16,7 +16,9 @@ namespace ofxChoc
     {
     public:
         using FileCallback = std::function<void(const std::string&)>;
-        using ColorCallback = std::function<void(const std::array<unsigned char, 3>&)>;
+        using ColorCallback = std::function<void(const ofColor&)>;
+        using InputCallback = std::function<void(const std::string&)>;
+        using MessageCallback = std::function<void()>;
 
         Dialogs() = default;
         ~Dialogs() {
@@ -26,21 +28,39 @@ namespace ofxChoc
         }
 
         void openFileDialog(const std::string& title, const std::string& defaultPath, const std::string& filter, FileCallback callback) {
-            if (isThreadRunning()) {
-                waitForThread(true);
-            }
-
-            this->title = title;
-            this->defaultPath = defaultPath;
-            this->filter = filter;
-            this->fileCallback = callback;
-            this->isSaveDialog = false;
-            this->isColorPicker = false;
-
-            startThread();
+            startDialogThread(title, defaultPath, filter, callback, false, false, false, false, false, false);
         }
 
         void saveFileDialog(const std::string& title, const std::string& defaultPath, const std::string& filter, FileCallback callback) {
+            startDialogThread(title, defaultPath, filter, callback, true, false, false, false, false, false);
+        }
+
+        void colorPickerDialog(const std::string& title, const ofColor& defaultColor, ColorCallback callback) {
+            this->defaultColor = defaultColor;
+            startDialogThread(title, "", "", nullptr, false, true, false, false, false, false);
+            this->colorCallback = callback;
+        }
+
+        void messageBox(const std::string& title, const std::string& message, const std::string& dialogType = "ok", MessageCallback callback = nullptr) {
+            this->message = message;
+            this->dialogType = dialogType;
+            startDialogThread(title, "", "", nullptr, false, false, true, false, false, false);
+            this->messageCallback = callback;
+        }
+
+        void inputBox(const std::string& title, const std::string& message, const std::string& defaultInput, InputCallback callback) {
+            this->message = message;
+            this->defaultInput = defaultInput;
+            startDialogThread(title, "", "", nullptr, false, false, false, true, false, false);
+            this->inputCallback = callback;
+        }
+
+        void chooseFolderDialog(const std::string& title, const std::string& defaultPath, FileCallback callback) {
+            startDialogThread(title, defaultPath, "", callback, false, false, false, false, true, false);
+        }
+
+    private:
+        void startDialogThread(const std::string& title, const std::string& defaultPath, const std::string& filter, FileCallback callback, bool isSaveDialog, bool isColorPicker, bool isMessageBox, bool isInputBox, bool isChooseFolder, bool isThreadRunningCheck) {
             if (isThreadRunning()) {
                 waitForThread(true);
             }
@@ -49,29 +69,24 @@ namespace ofxChoc
             this->defaultPath = defaultPath;
             this->filter = filter;
             this->fileCallback = callback;
-            this->isSaveDialog = true;
-            this->isColorPicker = false;
+            this->isSaveDialog = isSaveDialog;
+            this->isColorPicker = isColorPicker;
+            this->isMessageBox = isMessageBox;
+            this->isInputBox = isInputBox;
+            this->isChooseFolder = isChooseFolder;
 
             startThread();
         }
 
-        void colorPickerDialog(const std::string& title, const std::array<unsigned char, 3>& defaultColor, ColorCallback callback) {
-            if (isThreadRunning()) {
-                waitForThread(true);
-            }
-
-            this->title = title;
-            this->defaultColor = defaultColor;
-            this->colorCallback = callback;
-            this->isColorPicker = true;
-
-            startThread();
-        }
-
-    private:
         void threadedFunction() override {
             if (isColorPicker) {
                 colorPickerThread(title, defaultColor, colorCallback);
+            } else if (isMessageBox) {
+                messageBoxThread(title, message, dialogType, messageCallback);
+            } else if (isInputBox) {
+                inputBoxThread(title, message, defaultInput, inputCallback);
+            } else if (isChooseFolder) {
+                chooseFolderThread(title, defaultPath, fileCallback);
             } else if (isSaveDialog) {
                 fileDialogThread(title, defaultPath, filter, fileCallback, true);
             } else {
@@ -94,10 +109,10 @@ namespace ofxChoc
             stopThread();
         }
 
-        void colorPickerThread(const std::string& title, const std::array<unsigned char, 3>& defaultColor, ColorCallback callback) {
-            unsigned char color[3] = { defaultColor[0], defaultColor[1], defaultColor[2] };
+        void colorPickerThread(const std::string& title, const ofColor& defaultColor, ColorCallback callback) {
+            unsigned char color[3] = { defaultColor.r, defaultColor.g, defaultColor.b };
             if (tinyfd_colorChooser(title.c_str(), NULL, color, color)) {
-                std::array<unsigned char, 3> selectedColor = { color[0], color[1], color[2] };
+                ofColor selectedColor(color[0], color[1], color[2]);
                 callback(selectedColor);
             } else {
                 callback(defaultColor);
@@ -106,13 +121,49 @@ namespace ofxChoc
             stopThread();
         }
 
+        void messageBoxThread(const std::string& title, const std::string& message, const std::string& dialogType, MessageCallback callback) {
+            tinyfd_messageBox(title.c_str(), message.c_str(), dialogType.c_str(), "info", 1);
+            if (callback) {
+                callback();
+            }
+            stopThread();
+        }
+
+        void inputBoxThread(const std::string& title, const std::string& message, const std::string& defaultInput, InputCallback callback) {
+            const char* input = tinyfd_inputBox(title.c_str(), message.c_str(), defaultInput.c_str());
+            if (input) {
+                callback(input);
+            } else {
+                callback("");
+            }
+            stopThread();
+        }
+
+        void chooseFolderThread(const std::string& title, const std::string& defaultPath, FileCallback callback) {
+            const char* folderName = tinyfd_selectFolderDialog(title.c_str(), defaultPath.c_str());
+            if (folderName) {
+                callback(folderName);
+            } else {
+                callback("");
+            }
+            stopThread();
+        }
+
         std::string title;
         std::string defaultPath;
         std::string filter;
         FileCallback fileCallback;
         ColorCallback colorCallback;
-        std::array<unsigned char, 3> defaultColor;
+        InputCallback inputCallback;
+        MessageCallback messageCallback;
+        std::string message;
+        std::string defaultInput;
+        std::string dialogType;
+        ofColor defaultColor;
         bool isSaveDialog;
         bool isColorPicker;
+        bool isMessageBox;
+        bool isInputBox;
+        bool isChooseFolder;
     };
 }
