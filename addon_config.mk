3108be980ed6c017c40427af1eb4f8fadb9a3c31
Thomas Geissl <thomas.geissl@gmail.com>
2024-09-28 13:02:38 -0500

    changed description

--- addon_config.mk
+++ addon_config.mk
@@ -1,6 +1,6 @@
 meta:
 	ADDON_NAME = ofxChoc
-	ADDON_DESCRIPTION = ofxAddon including the choc library by Julian Storer (the juce guy)
+	ADDON_DESCRIPTION = ofxAddon including the choc library by Julian Storer/tracktion
 	ADDON_AUTHOR = Thomas Geissl
 	ADDON_TAGS = "some useful utilities, e.g. a webview class"
 	ADDON_URL = https://github.com/thomasgeissl/ofxChoc
